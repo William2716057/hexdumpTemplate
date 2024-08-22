1a34752ab6101199bb47de1cb45c58c2910cbefa
User <Willspri@gmail.com>
2024-08-23 11:40:43 +1200

    comments

--- hexdump.py
+++ hexdump.py
@@ -5,10 +5,13 @@ def hexdump():
     try: # open file in binary mode and output in write mode
         with open(file_path, 'rb') as file, open(output_path, 'w') as output_file:
             offset = 0 #initialise byte offset (position in file) to zero
-            chunk = file.read(16)  
+            chunk = file.read(16)  #chunk is first 16 bytes
+            #keep looping until no more data found
             while chunk: 
+                #each byte in chunk is converted to hexadecimal representation
                 hex_part = ' '.join(f'{byte:02x}' for byte in chunk)
-                ascii_part = ''.join(chr(byte) if 32 <= byte <= 126 else '.' for byte in chunk)
+                # ascii column 
+                ascii_part = ''.join(chr(byte) if 32 <= byte <= 126 else '.' for byte in chunk) # '.' means non printable
                 output_file.write(f'{offset:08x}  {hex_part:<47}  {ascii_part}\n')
                 offset += len(chunk)
                 chunk = file.read(16) 
